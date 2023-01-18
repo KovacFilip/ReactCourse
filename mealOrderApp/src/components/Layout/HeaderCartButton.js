b5f3e1fb7283ad99bde4293b521a419682c89829
KovacFilip <kovacfilipmh@gmail.com>
2023-01-18 22:28:49 +0100

    using context in cart button badge

--- mealOrderApp/src/components/Layout/HeaderCartButton.js
+++ mealOrderApp/src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import classes from './HeaderCartButton.module.css';
 import CartIcon from '../../assets/CartIcon';
+import { CartContext } from '../../store/cart-context';
 
 export const HeaderCartButton = (props) => {
+    const cartCtx = useContext(CartContext);
+
     return (
         <button className={classes.button} {...props}>
             <span className={classes.icon}>
                 <CartIcon />
             </span>
             <span>Your Cart</span>
-            <span className={classes.badge}>3</span>
+            <span className={classes.badge}>{cartCtx.items.length}</span>
         </button>
     );
 };

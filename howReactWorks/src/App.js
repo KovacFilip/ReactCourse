fb3d4baaae7a8b0eef33b8a58d5f75a1f55addd2
KovacFilip <kovacfilipmh@gmail.com>
2023-01-19 21:43:21 +0100

    Added a button to display paragraph

--- howReactWorks/src/App.js
+++ howReactWorks/src/App.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './App.css';
+import Button from './components/UI/Button/Button';
 
 function App() {
-  return (
-    <div className="app">
-      <h1>Hi there!</h1>
-    </div>
-  );
+    const [showParagraph, setShowParagraph] = useState(false);
+
+    const toggleParagraphHandler = () => {
+        setShowParagraph((prevState) => {
+            return !prevState;
+        });
+    };
+
+    return (
+        <div className="app">
+            <h1>Hi there!</h1>
+            {showParagraph && <p>This is new!</p>}
+            <Button onClick={toggleParagraphHandler}>Show Paragraph</Button>
+        </div>
+    );
 }
 
 export default App;

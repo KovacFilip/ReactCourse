05f29047fe831575bef28e3601a9a16029fe752a
KovacFilip <kovacfilipmh@gmail.com>
2023-03-05 23:29:38 +0100

    updated dependencies in custom hook

--- customHooksVol2/src/hooks/UseHttpRequest.js
+++ customHooksVol2/src/hooks/UseHttpRequest.js
@@ -11,53 +11,59 @@ const useHttpRequest = ({
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(null);
 
-    const makeFetchRequest = useCallback(async (taskText) => {
-        setIsLoading(true);
-        setError(null);
+    const makeFetchRequest = useCallback(
+        async (taskText) => {
+            setIsLoading(true);
+            setError(null);
 
-        try {
-            let response = null;
+            try {
+                let response = null;
 
-            if (post) {
-                response = await fetch(FIREBASE_API_URL, {
-                    method: 'POST',
-                    body: JSON.stringify({ text: taskText }),
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                });
-            } else {
-                response = await fetch(
-                    'https://react-http-d4588-default-rtdb.europe-west1.firebasedatabase.app/tasks.json'
-                );
-            }
-
-            if (!response.ok) {
-                throw new Error('Request failed!');
-            }
-
-            const data = await response.json();
-
-            if (post) {
-                const generatedId = data.name; // firebase-specific => "name" contains generated id
-                const createdTask = { id: generatedId, text: taskText };
-
-                onAddTask(createdTask);
-            } else {
-                const loadedTasks = [];
-
-                for (const taskKey in data) {
-                    loadedTasks.push({ id: taskKey, text: data[taskKey].text });
+                if (post) {
+                    response = await fetch(FIREBASE_API_URL, {
+                        method: 'POST',
+                        body: JSON.stringify({ text: taskText }),
+                        headers: {
+                            'Content-Type': 'application/json',
+                        },
+                    });
+                } else {
+                    response = await fetch(
+                        'https://react-http-d4588-default-rtdb.europe-west1.firebasedatabase.app/tasks.json'
+                    );
                 }
 
-                setTasks(loadedTasks);
-            }
-        } catch (err) {
-            setError(err.message || 'Something went wrong!');
-        }
+                if (!response.ok) {
+                    throw new Error('Request failed!');
+                }
 
-        setIsLoading(false);
-    }, []);
+                const data = await response.json();
+
+                if (post) {
+                    const generatedId = data.name; // firebase-specific => "name" contains generated id
+                    const createdTask = { id: generatedId, text: taskText };
+
+                    onAddTask(createdTask);
+                } else {
+                    const loadedTasks = [];
+
+                    for (const taskKey in data) {
+                        loadedTasks.push({
+                            id: taskKey,
+                            text: data[taskKey].text,
+                        });
+                    }
+
+                    setTasks(loadedTasks);
+                }
+            } catch (err) {
+                setError(err.message || 'Something went wrong!');
+            }
+
+            setIsLoading(false);
+        },
+        [onAddTask, post, setTasks]
+    );
 
     return [isLoading, error, makeFetchRequest];
 };

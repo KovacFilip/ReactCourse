357f531f252fe70fd5b1829656a7dfd4b55a0002
KovacFilip <kovacfilipmh@gmail.com>
2023-03-30 00:08:19 +0200

    Getting the meal data from firebase db

--- mealOrderApp/src/components/Meals/AvailableMeals.js
+++ mealOrderApp/src/components/Meals/AvailableMeals.js
@@ -1,52 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getMeals } from '../../backend/firebaseFunctions';
 import { Card } from '../UI/Card';
-import { MealItem } from './MealItem/MealItem';
 import classes from './AvailableMeals.module.css';
-
-const DUMMY_MEALS = [
-    {
-        id: 'm1',
-        name: 'Sushi',
-        description: 'Finest fish and veggies',
-        price: 22.99,
-    },
-    {
-        id: 'm2',
-        name: 'Schnitzel',
-        description: 'A german specialty!',
-        price: 16.5,
-    },
-    {
-        id: 'm3',
-        name: 'Barbecue Burger',
-        description: 'American, raw, meaty',
-        price: 12.99,
-    },
-    {
-        id: 'm4',
-        name: 'Green Bowl',
-        description: 'Healthy...and green...',
-        price: 18.99,
-    },
-];
+import { MealItem } from './MealItem/MealItem';
 
 export const AvailableMeals = () => {
-    const mealsList = DUMMY_MEALS.map((el) => {
-        return (
-            <MealItem
-                name={el.name}
-                desc={el.description}
-                price={el.price}
-                id={el.id}
-                key={el.id}
-            />
-        );
-    });
+    const [loaded, setLoaded] = useState(false);
+    const [meals, setMeals] = useState({});
+
+    useEffect(() => {
+        getMeals()
+            .then((res) => {
+                console.log(res.data);
+                setMeals(
+                    res.data.map((el) => {
+                        return (
+                            <MealItem
+                                name={el.name}
+                                desc={el.description}
+                                price={el.price}
+                                id={el.id}
+                                key={el.id}
+                            />
+                        );
+                    })
+                );
+                setLoaded(true);
+            })
+            .catch((err) => console.log(err));
+    }, []);
+
+    if (!loaded) {
+        return <div>Loading</div>;
+    }
 
     return (
         <section className={classes.meals}>
             <Card>
-                <ul>{mealsList}</ul>
+                <ul>{meals}</ul>
             </Card>
         </section>
     );

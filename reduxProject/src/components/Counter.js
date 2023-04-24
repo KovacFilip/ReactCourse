d9fa50d64b8fb0a22f3edf3d83d493a55a33283c
KovacFilip <kovacfilipmh@gmail.com>
2023-04-24 23:47:53 +0200

    Basic redux finish up

--- reduxProject/src/components/Counter.js
+++ reduxProject/src/components/Counter.js
@@ -1,28 +1,39 @@
 import { useDispatch, useSelector } from 'react-redux';
+
+import {
+    decrement,
+    increase,
+    increment,
+    toggleCounter,
+} from '../store/counterSlice';
+
 import classes from './Counter.module.css';
 
 const Counter = () => {
     const dispatch = useDispatch();
-    const counter = useSelector((state) => state.counter);
+    const counter = useSelector((state) => state.counter.counter);
+    const showCounter = useSelector((state) => state.counter.showCounter);
 
     const incrementHandler = () => {
-        dispatch({ type: 'increment' });
+        dispatch(increment());
     };
 
     const increaseHandler = () => {
-        dispatch({ type: 'increase', amount: 5 });
+        dispatch(increase({ amount: 5 }));
     };
 
     const decrementHandler = () => {
-        dispatch({ type: 'decrement' });
+        dispatch(decrement());
     };
 
-    const toggleCounterHandler = () => {};
+    const toggleCounterHandler = () => {
+        dispatch(toggleCounter());
+    };
 
     return (
         <main className={classes.counter}>
             <h1>Redux Counter</h1>
-            <div className={classes.value}>{counter}</div>
+            {showCounter && <div className={classes.value}>{counter}</div>}
             <div>
                 <button onClick={incrementHandler}>Increment</button>
                 <button onClick={increaseHandler}>IncrementBy5</button>
--- reduxProject/src/store/index.js
+++ reduxProject/src/store/index.js
@@ -1,21 +1,9 @@
-import { createStore } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
+import authReducer from './authSlice';
+import counterReducer from './counterSlice';
 
-const counterReducer = (state = { counter: 0 }, action) => {
-    if (action.type === 'increment') {
-        return { counter: state.counter + 1 };
-    }
-
-    if (action.type === 'increase') {
-        return { counter: state.counter + action.amount };
-    }
-
-    if (action.type === 'decrement') {
-        return { counter: state.counter - 1 };
-    }
-
-    return state;
-};
-
-const store = createStore(counterReducer);
+const store = configureStore({
+    reducer: { counter: counterReducer, auth: authReducer },
+});
 
 export default store;

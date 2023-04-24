31feb4746c765275ec979e4fa5855872adadab45
KovacFilip <kovacfilipmh@gmail.com>
2023-04-24 22:16:50 +0200

    Working with payload

--- reduxProject/src/store/index.js
+++ reduxProject/src/store/index.js
@@ -3,7 +3,13 @@ import { createStore } from 'redux';
 const counterReducer = (state = { counter: 0 }, action) => {
     if (action.type === 'increment') {
         return { counter: state.counter + 1 };
-    } else if (action.type === 'decrement') {
+    }
+
+    if (action.type === 'increase') {
+        return { counter: state.counter + action.amount };
+    }
+
+    if (action.type === 'decrement') {
         return { counter: state.counter - 1 };
     }
 

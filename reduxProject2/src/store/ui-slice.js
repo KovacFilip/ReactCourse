38de4249443c93efa16781541feeb0507c055d7b
KovacFilip <kovacfilipmh@gmail.com>
2023-05-18 22:45:23 +0200

    Added notifications basic

--- reduxProject2/src/store/ui-slice.js
+++ reduxProject2/src/store/ui-slice.js
@@ -1,14 +1,20 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 const uiSlice = createSlice({
-    name: 'ui',
-    initialState: { cartIsVisible: false },
+    name: "ui",
+    initialState: { cartIsVisible: false, notification: null },
     reducers: {
         toggleVisible(state, action) {
             state.cartIsVisible = !state.cartIsVisible;
         },
+        setNotification(state, action) {
+            state.notification = {
+                status: action.payload.status,
+                title: action.payload.message,
+            };
+        },
     },
 });
 
-export const { toggleVisible } = uiSlice.actions;
+export const { toggleVisible, setNotification } = uiSlice.actions;
 export default uiSlice.reducer;
